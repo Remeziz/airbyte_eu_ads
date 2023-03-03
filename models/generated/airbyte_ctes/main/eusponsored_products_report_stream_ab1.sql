c20a8b35a52f91577173ac851761c67f64b27015
Remeziz <79668248+Remeziz@users.noreply.github.com>
2023-03-03 14:58:52 +0200

    Update eusponsored_products_report_stream_ab1.sql

--- models/generated/airbyte_ctes/main/eusponsored_products_report_stream_ab1.sql
+++ models/generated/airbyte_ctes/main/eusponsored_products_report_stream_ab1.sql
@@ -4,7 +4,7 @@
     tags = [ "top-level-intermediate" ]
 ) }}
 -- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
--- depends_on: {{ source('main', '_airbyte_raw_eusponso__roducts_report_stream') }}
+-- depends_on: {{ source('main', '_airbyte_raw_eu_sponso__roducts_report_stream') }}
 select
     {{ json_extract('table_alias', '_airbyte_data', ['metric'], ['metric']) }} as metric,
     {{ json_extract_scalar('_airbyte_data', ['profileId'], ['profileId']) }} as profileid,
@@ -14,8 +14,8 @@ select
     _airbyte_ab_id,
     _airbyte_emitted_at,
     {{ current_timestamp() }} as _airbyte_normalized_at
-from {{ source('main', '_airbyte_raw_eusponso__roducts_report_stream') }} as table_alias
--- eusponsored_products_report_stream
+from {{ source('main', '_airbyte_raw_eu_sponso__roducts_report_stream') }} as table_alias
+-- eu_sponsored_products_report_stream
 where 1 = 1
 {{ incremental_clause('_airbyte_emitted_at', this) }}
 
