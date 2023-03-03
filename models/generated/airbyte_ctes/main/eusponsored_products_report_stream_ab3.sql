8b91c2d190413d3138846732a05e3f13fb7590a6
Remeziz <79668248+Remeziz@users.noreply.github.com>
2023-03-03 14:59:50 +0200

    Update eusponsored_products_report_stream_ab3.sql

--- models/generated/airbyte_ctes/main/eusponsored_products_report_stream_ab3.sql
+++ models/generated/airbyte_ctes/main/eusponsored_products_report_stream_ab3.sql
@@ -4,7 +4,7 @@
     tags = [ "top-level-intermediate" ]
 ) }}
 -- SQL model to build a hash column based on the values of this record
--- depends_on: {{ ref('eusponsored_products_report_stream_ab2') }}
+-- depends_on: {{ ref('eu_sponsored_products_report_stream_ab2') }}
 select
     {{ dbt_utils.surrogate_key([
         object_to_string('metric'),
@@ -12,10 +12,10 @@ select
         'updatedat',
         'recordtype',
         'reportdate',
-    ]) }} as _airbyte_eusponsored____report_stream_hashid,
+    ]) }} as _airbyte_eu_sponsored____report_stream_hashid,
     tmp.*
-from {{ ref('eusponsored_products_report_stream_ab2') }} tmp
--- eusponsored_products_report_stream
+from {{ ref('eu_sponsored_products_report_stream_ab2') }} tmp
+-- eu_sponsored_products_report_stream
 where 1 = 1
 {{ incremental_clause('_airbyte_emitted_at', this) }}
 
